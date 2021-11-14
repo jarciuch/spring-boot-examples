34f2ae5815958c2cf8d4afbd1d766d420af0c9d0
Jan Arciuchiewicz <jarciuch@gmail.com>
2021-11-14 11:29:01 +0000

    Initial commit of spring-rest-db-example

---
+++ spring-rest-db-example/src/main/java/com/shortmile/springexamples/restdb/SpringRestDbExampleApplication.java
@@ -0,0 +1,14 @@
+package com.shortmile.springexamples.restdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringRestDbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDbExampleApplication.class, args);
+	}
+
+}
---
+++ spring-rest-db-example/src/test/java/com/shortmile/springexamples/restdb/SpringRestDbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortmile.springexamples.restdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDbExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
