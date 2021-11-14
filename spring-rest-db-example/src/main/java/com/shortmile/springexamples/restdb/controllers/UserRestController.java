ae6502ba834ef5d95684b588b42192e3825bc50c
Jan Arciuchiewicz <jarciuch@gmail.com>
2021-11-14 23:32:12 +0000

    Rename packages to plural form

--- spring-rest-db-example/src/main/java/com/shortmile/springexamples/restdb/controllers/UserRestController.java
+++ spring-rest-db-example/src/main/java/com/shortmile/springexamples/restdb/controllers/UserRestController.java
@@ -1,6 +1,6 @@
-package com.shortmile.springexamples.restdb.controller;
+package com.shortmile.springexamples.restdb.controllers;
 
-import com.shortmile.springexamples.restdb.resource.User;
+import com.shortmile.springexamples.restdb.resources.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +8,8 @@ import java.util.List;
 
 @RestController
 public class UserRestController {
-    List<User> users = List.of(new User(1L, "John Doe", "john.doe@email.com"));
+
+    List<User> users = List.of(User.builder().id(1L).name("John").email("john@email").build());
 
 
     @GetMapping("/users")
--- spring-rest-db-example/src/main/java/com/shortmile/springexamples/restdb/resources/User.java
+++ spring-rest-db-example/src/main/java/com/shortmile/springexamples/restdb/resources/User.java
@@ -1,8 +1,9 @@
-package com.shortmile.springexamples.restdb.resource;
+package com.shortmile.springexamples.restdb.resources;
 
 import lombok.*;
 
 @Data
+@Builder
 public class User {
     private final Long id;
     private final String name;
